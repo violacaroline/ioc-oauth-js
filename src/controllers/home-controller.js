4f7065e5eddcd8623bee043865ead306348edcea
Caroline Akesson <ca223pw@student.lnu.se>
2023-03-03 12:51:24 -0500

    Successfully authorize user with Gitlab

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -13,4 +13,16 @@ export class HomeController {
   index (req, res, next) {
     res.render('home/index')
   }
+
+  redirectToGitlab(req, res, next) {
+    res.redirect(`https://gitlab.lnu.se/oauth/authorize?client_id=${process.env.APPLICATION_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&state=${process.env.STATE}&scope=read_user+read_api`)
+  }
+
+  successAuthorization(req, res, next) {
+    res.render('authorized/welcome')
+  }
+
+  profile(req, res, next) {
+    res.render('authorized/profile')
+  }
 }
